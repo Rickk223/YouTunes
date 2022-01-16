e58c24b5d07faeceb6dfbd923a6f5e6fad6cbbc4
Ricardo Guillen <rguillenvergara@my365.bellevue.edu>
2022-01-16 09:36:49 -0800

    module 3 sprint

---
+++ youtunes/src/main/java/youtunes/model/db_queries.sql
@@ -0,0 +1,21 @@
+/*
+    Title: queries.sql
+    Author: Ricardo Guillen Vergara
+    Date: 1/15/2022
+    Description: youtunes database queires for index.jsp file
+*/
+
+SELECT al.title, art.last_name FROM album al 
+	INNER JOIN artist art ON al.artist_id = art.artist_id;
+	
+	
+-- Query to populate the index.jsp page
+SELECT al.album_id, 
+	   al.title, 
+	   al.price, 
+	   al.genre, 
+	   ar.last_name, 
+	   ar.artist_id 
+FROM album al
+INNER JOIN 
+	artist ar ON al.artist_id = ar.artist_id;
